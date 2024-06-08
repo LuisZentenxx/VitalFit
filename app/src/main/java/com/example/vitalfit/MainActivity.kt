655dce366895c71595badf4149ff61c1db6c0f83
Fernan_dr0id <luis.zenteno02@inacapmail.cl>
2024-06-08 18:08:05 -0400

    add gender selected function

--- app/src/main/java/com/example/vitalfit/MainActivity.kt
+++ app/src/main/java/com/example/vitalfit/MainActivity.kt
@@ -2,12 +2,67 @@ package com.example.vitalfit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initComponent()
+        initListener()
+        initUI()
+
+    }
+
+    //Inicializa los componentes
+    private fun initComponent() {
+        viewMale = findViewById(R.id.card1)
+        viewFemale = findViewById(R.id.card2)
+    }
+
+    //Accede a cada vista
+    private fun initListener() {
+        viewMale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
         }
     }
+
+    private fun changeGender(){
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+    }
+
+    private fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+    }
+
+    private fun getBackgroundColor(isSelectedComponent: Boolean) : Int {
+        val colorReference = if(isSelectedComponent){
+            R.color.selected
+        }else{
+            R.color.is_selected
+        }
+
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initUI() {
+        setGenderColor()
+    }
+}
--- app/src/main/java/com/example/vitalfit/SplashActivity.kt
+++ app/src/main/java/com/example/vitalfit/SplashActivity.kt
@@ -1,14 +1,17 @@
 package com.example.vitalfit
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,11 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         // Splash Screen
         Handler(Looper.getMainLooper()).postDelayed({
