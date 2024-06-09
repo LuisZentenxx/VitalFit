02bc1465cedf177927a6232dba5584ca7be8ad52
Fernan_dr0id <luis.zenteno02@inacapmail.cl>
2024-06-08 21:47:37 -0400

    design layout

--- app/src/main/java/com/example/vitalfit/MainActivity.kt
+++ app/src/main/java/com/example/vitalfit/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vitalfit
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -17,6 +18,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
 
         initComponent()
         initListener()
